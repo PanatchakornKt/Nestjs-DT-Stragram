c5ae89a40461771fc28bbd0fe8bcb672f6f7c5ec
Panatchakorn Worachotchanapat <panatchakorn@daytech.co.th>
2021-06-16 11:41:51 +0700

    edit endpoint comment

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,43 +1,43 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { PostsRepository } from './posts.repository';
-import { CreatePostsDto } from './dto/create-posts-dto';
-import * as fsExtra from 'fs-extra';
+// import { Injectable, NotFoundException } from '@nestjs/common';
+// import { InjectRepository } from '@nestjs/typeorm';
+// import { PostsRepository } from './posts.repository';
+// import { CreatePostsDto } from './dto/create-posts-dto';
+// import * as fsExtra from 'fs-extra';
 
-@Injectable()
-export class PostsService {
-  constructor(
-    @InjectRepository(PostsRepository) private postsRepository: PostsRepository,
-  ) {}
+// @Injectable()
+// export class PostsService {
+//   constructor(
+//     @InjectRepository(PostsRepository) private postsRepository: PostsRepository,
+//   ) {}
 
-  async createPosts(createPostsDto: CreatePostsDto) {
-    return await this.postsRepository.createPosts(createPostsDto);
-  }
+//   async createPosts(createPostsDto: CreatePostsDto) {
+//     return await this.postsRepository.createPosts(createPostsDto);
+//   }
 
-  async getPostsTxt() {
-    return await this.postsRepository.find();
-  }
+//   async getPostsTxt() {
+//     return await this.postsRepository.find();
+//   }
 
-  async getPostsById(post_id: number) {
-    const found = await this.postsRepository.findOne(post_id);
-    if (!found) {
-      throw new NotFoundException(`Post ${post_id} is not found`);
-    }
-    return found;
-  }
+//   async getPostsById(post_id: number) {
+//     const found = await this.postsRepository.findOne(post_id);
+//     if (!found) {
+//       throw new NotFoundException(`Post ${post_id} is not found`);
+//     }
+//     return found;
+//   }
 
-  async deletePosts(post_id: number) {
-    const found = await this.getPostsById(post_id);
-    const { post_img } = found;
-    await fsExtra.remove(`upload/${post_img}`);
-    return await this.postsRepository.delete(post_id);
-  }
+//   async deletePosts(post_id: number) {
+//     const found = await this.getPostsById(post_id);
+//     const { post_img } = found;
+//     await fsExtra.remove(`upload/${post_img}`);
+//     return await this.postsRepository.delete(post_id);
+//   }
 
-  async updatePosts(post_id: number, createPostsDto: CreatePostsDto) {
-    const post = await this.getPostsById(post_id);
-    const { post_text } = createPostsDto;
-    post.post_text = post_text;
-    await post.save();
-    return post;
-  }
-}
+//   async updatePosts(post_id: number, createPostsDto: CreatePostsDto) {
+//     const post = await this.getPostsById(post_id);
+//     const { post_text } = createPostsDto;
+//     post.post_text = post_text;
+//     await post.save();
+//     return post;
+//   }
+// }
